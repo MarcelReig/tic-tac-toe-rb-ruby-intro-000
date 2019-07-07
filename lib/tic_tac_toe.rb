3151cd25fe81d4d9a314512f5186cd6102c870d0
Marcel Reig <marcelreig@gmail.com>
2019-07-07 16:26:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,14 +45,15 @@ def position_taken?(board, index)
   board[index] != " "
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+def turn
+   puts "Please enter 1-9:"
+   input = gets.strip
+   index = input.to_i - 1
+   if valid_move?(input)
+     player = current_player
+     move(input, player)
+     display_board
+   else
+     turn
+   end
+ end
